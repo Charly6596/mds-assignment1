745e10b4c9eeb7e1442d8bbf841897930d00bde4
charly6595 <cgutisanc@gmail.com>
2022-03-09 22:55:02 +0100

    first commit

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,10 @@
+/**
+ * Class providing a method that computes the factorial of
+ * an integer number
+ * @author Carlos Javier Gutiérrez Sánchez
+ */
+public class Factorial {
+    public int compute(int n) {
+        return 0;
+    }
+}
